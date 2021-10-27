a294ea595cb5b498f68269d27599f02ae4264e6a
lucass <24lspeier@athenian.org>
2021-10-27 10:05:04 -0700

    sloped

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -3,4 +3,34 @@ public class BankAccount {
     private int balance;
     private int accountNumber;
     private String preferredName;
+    public BankAccount(int balance2, int accountNumber2, String preferredName2) {
+        balance = balance2;
+        accountNumber = accountNumber2;
+        preferredName = preferredName2;
+    }
+    public void withdraw(int x) {
+        if (balance >= x) {
+            balance -= x;
+            System.out.println(preferredName+" withdrawed $"+x);
+        }
+    }
+    public void deposit(int x) {
+        balance += x;
+        System.out.println(preferredName+" deposited $"+x);
+    }
+    public int getBalance() {
+        return balance;
+    }
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+    public String getPrefferedName() {
+        return preferredName;
+    }
+    public void setPreferredName(String preferredName2) {
+        preferredName = preferredName2;
+    }
+    public String toString() {
+        return preferredName+": $"+balance;
+    }
 }
--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 public class BankClient {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("What is your preferred name?");
-        String name = scan.nextLine();
+        BankAccount myBankAccount = new BankAccount(5000, 234, "Lucas");
+        System.out.println(myBankAccount.getBalance()); // 5000
+        myBankAccount.withdraw(200); // Lucas withdrawed $200
+        System.out.println(myBankAccount.getBalance()); // 4800
+        myBankAccount.deposit(500); // Lucas deposited $500
+        System.out.println(myBankAccount.getBalance()); // 5300
+        System.out.println(myBankAccount.getAccountNumber()); // 234
+        System.out.println(myBankAccount.getPrefferedName()); // Lucas
+        myBankAccount.setPreferredName("Test");
+        System.out.println(myBankAccount.getPrefferedName()); // Test
+        System.out.println(myBankAccount); // Test: 5300
 //        // Create a bank account with a balance of 0, the account number 101134, and the given name
 //        BankAccount account = new BankAccount(0, 101134, name);
     }
